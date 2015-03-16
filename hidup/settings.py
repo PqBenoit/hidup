afbd95837ae786bceadf847d3664bdd7320d09b1
PqBenoit <lpdw@MC-GE-ST-C121-11.local>
2015-03-16 11:20:51 +0100

    setting mongo as database engine

--- hidup/settings.py
+++ hidup/settings.py
@@ -57,10 +57,10 @@ WSGI_APPLICATION = 'hidup.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default' : {
+      'ENGINE' : 'django_mongodb_engine',
+      'NAME' : 'hidup'
+   }
 }
 
 # Internationalization
